d602237ef52ce1d5d2359dbca0c107e5271d8524
Vika088 <danilovaviktoriya088@gmail.com>
2024-10-15 18:55:15 -0800

    second commit

--- Test/test_api.py
+++ Test/test_api.py
@@ -1,7 +1,11 @@
+import allure
 import requests
 from data import *
 
 
+@allure.epic('API. Поиск фильмов')
+@allure.story('API. Проверка поиска фильма')
+@allure.title('API. Поиск по названию фильма')
 def test_api_search_by_name():
     query_params = {
         'page': '1',
@@ -13,6 +17,10 @@ def test_api_search_by_name():
     resp = requests.get(URL + '/v1.4/movie/search', json=query_params, headers=my_headers)
     assert resp.status_code == 200
 
+
+@allure.epic('API. Поиск фильмов')
+@allure.story('API. Проверка поиска фильма')
+@allure.title('API. Поиск фильма по ID')
 def test_api_search_by_id():
     query_params = {
         'id': '1316601'
@@ -22,6 +30,10 @@ def test_api_search_by_id():
     resp = requests.get(URL + '/v1.4/movie', json=query_params, headers=my_headers)
     assert resp.status_code == 200
 
+
+@allure.epic('API. Поиск фильмов')
+@allure.story('API. Проверка поиска фильма')
+@allure.title('API. Поиск по фильма  по актеру')
 def test_api_search_by_actors_name():
     query_params = {
         'page': '1',
@@ -33,22 +45,28 @@ def test_api_search_by_actors_name():
     resp = requests.get(URL + '/v1.4/person/search', json=query_params, headers=my_headers)
     assert resp.status_code == 200
 
+
+@allure.epic('API. Поиск фильмов')
+@allure.story('API. Проверка поиска фильма')
+@allure.title('API. Поиск по названию фильма с неправильными параметрами')
 def test_api_search_by_wrong_query():
-    query_params = {
+    params = {
         'page': '1',
         'limit': '',
         'query': 'House of the dragon'
     }
     my_headers = {"X-API-KEY": token}
 
-    resp = requests.get(URL + '/v1.4/movie/search', json=query_params, headers=my_headers)
+    resp = requests.get(URL + '/v1.4/movie/search', params=params, headers=my_headers)
     assert resp.status_code == 400
 
+
+@allure.epic('API. Поиск фильмов')
+@allure.story('API. Проверка поиска фильма')
+@allure.title('API. Поиск фильма по несуществующему ID')
 def test_api_search_by_id_doesnt_exist():
-    query_params = {
-        'id': '1000100 1010110 1010010'
-    }
+    params = {'id': '1000100 1010110 1010010'}
     my_headers = {"X-API-KEY": token}
 
-    resp = requests.get(URL + '/v1.4/movie', json=query_params, headers=my_headers)
+    resp = requests.get(URL + '/v1.4/movie', params=params, headers=my_headers)
     assert resp.status_code == 400
